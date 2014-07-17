837e98150dac8fba755665f9cfdb6c6d1d6d2bc5
surajshetiya <surajshetiya@gmail.com>
2014-07-17 19:27:00 +0530

    Working code added to prototype

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,21 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        get <- function(){
+                x
+        }
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        getInv <- function(){
+                inv
+        }
+        setInv <- function(inverse) {
+                inv <<- inverse
+        }
+        list(set = set, get = get,getInv = getInv,setInv = setInv);
 }
 
 
@@ -12,4 +26,22 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        Inverse <- x$getInv()
+        if(is.null(Inverse)){
+                Inverse <- solve(x$get())
+                x$setInv(Inverse)
+        }
+        Inverse
 }
+
+## Output:
+##
+## > mat <- matrix(sample(1:1000000,1000000,replace=T),nrow=1000,ncol=1000)
+## > mat_obj <- makeCacheMatrix(mat)
+## > system.time(Inv <- cacheSolve(mat_obj))
+## user  system elapsed 
+## 3.01    0.03    3.11 
+## > system.time(Inv <- cacheSolve(mat_obj))
+## user  system elapsed 
+## 0       0       0 
+##> 
